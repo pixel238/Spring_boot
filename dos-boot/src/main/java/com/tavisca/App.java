53fcc3d1f61b481ca3d6c6fc3f0fab8bf7cc7acd
tavisca-cchowdhary <cchowdhary@tavisca.in>
2019-09-23 14:22:13 +0530

    Implemented a restful api

---
+++ dos-boot/src/main/java/com/tavisca/App.java
@@ -0,0 +1,18 @@
+package com.tavisca;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+    }
+}
