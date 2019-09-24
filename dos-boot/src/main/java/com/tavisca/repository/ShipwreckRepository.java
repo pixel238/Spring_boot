21798a25b34aebf7bd9b7b37216ff02ade603460
tavisca-cchowdhary <cchowdhary@tavisca.in>
2019-09-24 14:43:14 +0530

    Added a database connection with the app

---
+++ dos-boot/src/main/java/com/tavisca/repository/ShipwreckRepository.java
@@ -0,0 +1,9 @@
+package com.tavisca.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tavisca.model.Shipwreck;
+
+public interface ShipwreckRepository extends JpaRepository<Shipwreck,Long>{
+
+}
