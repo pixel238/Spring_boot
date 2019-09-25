8facd101ad8ceb8547eb7566232a6feaa0abd48b
tavisca-cchowdhary <cchowdhary@tavisca.in>
2019-09-25 10:39:21 +0530

    implemented unit test and mockito

--- dos-boot/src/test/java/com/tavisca/AppTest.java
+++ dos-boot/src/test/java/com/tavisca/AppTest.java
@@ -1,38 +1,18 @@
 package com.tavisca;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
 
-/**
- * Unit test for simple App.
- */
+import org.junit.Test;
+
+import com.tavisca.controller.HomeController;
+
 public class AppTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
+	@Test
     public void testApp()
     {
-        assertTrue( true );
+		HomeController hc = new HomeController();
+		String result = hc.home();
+		assertEquals(result, "Dos Boot, reporting for duty");
     }
 }
---
+++ dos-boot/src/test/java/com/tavisca/ShipwreckControllerTest.java
@@ -0,0 +1,40 @@
+package com.tavisca;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import static org.mockito.Mockito.*;
+
+import com.tavisca.controller.ShipwreckController;
+import com.tavisca.model.Shipwreck;
+import com.tavisca.repository.ShipwreckRepository;
+
+public class ShipwreckControllerTest {
+	@InjectMocks
+	private ShipwreckController sc;
+
+	@Mock
+	private ShipwreckRepository shipwreckRepository;
+	
+	@Before
+	public void init() {
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void testShipwreckGet() {
+		Shipwreck sw =new Shipwreck();
+		sw.setId(1L);
+		when(shipwreckRepository.findOne(1L)).thenReturn(sw);
+		
+		Shipwreck wreck = sc.get(1L);
+		
+		verify(shipwreckRepository).findOne(1L);
+		
+		assertEquals(1L, wreck.getId().longValue());
+	}
+}
